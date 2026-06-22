8ecc5199b4350499890196513ea4055268d1a165
ErnestoJVF <ernestojvf@outlook.com>
2026-06-22 09:45:31 -0500

    comentarios

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const caras = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
 
 const frecuencias = [0, 0, 0, 0, 0, 0];
 
-function lanzarDado() {
+function lanzarDado() { /*se ejecuta cuando le das click al boton lanzar dado del html*/
 
     // Genera un número pseudoaleatorio entre 1 y 6
-    let resultado = Math.floor(Math.random() * 6) + 1;
-
+    let resultado = Math.floor(Math.random() * 6) + 1; /*Math.floor es una función de JavaScript que redondea un número hacia abajo al entero más cercano. */
+                                                       /*es una función de JavaScript que genera un número decimal pseudoaleatorio entre 0 (incluido) y 1*/
     // Mostrar resultado numérico
-    document.getElementById("resultado").textContent = resultado;
+    document.getElementById("resultado").textContent = resultado; /*Muestra el número en el html*/
 
     // Mostrar la cara del dado
     document.getElementById("dado").textContent = caras[resultado - 1];
